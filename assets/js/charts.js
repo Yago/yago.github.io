29b2d51905f237d5c4466486f70af5175976d62b
Yann Gouffon <github@yago-art.ch>
2016-06-24 11:42:37 +0200

    change stats api

--- assets/js/charts.js
+++ assets/js/charts.js
@@ -7,7 +7,7 @@ var charts = function ($) {
   $(document).ready(function () {
     if ($('#pie-chart').length > 0) {
       var $pieChart = $('#pie-chart'),
-          data = 'http://codeivate.com/users/yago.json',
+          data = 'http://api.yago.io/stats/',
           shade = function (color, percent){
             var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
             return '#'+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
