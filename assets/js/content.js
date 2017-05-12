aeb107231dfb6791433b83ec92ea5a982088ae62
Yann Gouffon <github@yago-art.ch>
2017-05-12 16:21:19 +0200

    📝 add RAW blogpost

--- assets/js/content.js
+++ assets/js/content.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 
 export default () => {
   if ($('#article-container').length > 0) {
-    $('#article-container').find('pre + blockquote a').each(function () {
+    $('#article-container').find('a').each(function () {
       $(this).attr('target', '_blank');
     });
   }
