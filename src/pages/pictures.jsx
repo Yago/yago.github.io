bf8fd18fa77f288e8c6e966e04a04dbbbf91f0d9
Yann Gouffon <github@yago-art.ch>
2019-12-06 14:24:52 +0100

    ✨ add progressive loading pictures

--- src/pages/pictures.jsx
+++ src/pages/pictures.jsx
@@ -1,46 +1,70 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import PhotoGallery from 'react-photo-gallery';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import PhotoSwipe from 'photoswipe/dist/photoswipe';
-import PhotoSwipeUI_Default from 'photoswipe/dist/photoswipe-ui-default';
+import PhotoSwipeUIDefault from 'photoswipe/dist/photoswipe-ui-default';
+import Loader from 'react-loader-spinner';
 import { graphql } from 'gatsby';
 import Fade from 'react-reveal/Fade';
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
 import Layout from '../components/layout';
 import Icon from '../components/Icon';
 import SEO from '../components/seo';
 import PhotoswipeWrapper from '../components/content/PhotoswipeWrapper';
-
 import { formatTitle } from '../helpers/pictures';
 import pictures from '../config/pictures.json';
 
 const PicturesPage = ({ data, location }) => {
+  const [length, setLength] = useState(0);
+  const [collection, setCollection] = useState([]);
+  const photoswipeContainer = useRef([]);
+  const photoGalleryContainer = useRef([]);
+
   const currentWidth = typeof window !== 'undefined' ? window.innerWidth : 1000;
 
-  // Create container for photoswipe
-  const photoswipeContainer = pictures
-    .sort((a, b) => b.id - a.id)
-    .map((pic) => {
-      const images = data.allImageSharp.edges;
-      const image = images.find(edge => edge.node.fluid.src.includes(pic.id));
-      return {
-        ...image,
-        src: image.node.fluid.originalImg,
-        title: formatTitle(pic),
-        w: pic.w,
-        h: pic.h,
-      };
-    });
+  const addPictures = qty => {
+    console.log('more!', length, length + qty);
 
-  // Create container for react-photo-gallery
-  const photoGalleryContainer = photoswipeContainer.map(pic => ({
-    src: pic.node.fluid.src,
-    srcSet: pic.node.fluid.srcSet,
-    sizes: pic.node.fluid.sizes,
-    width: pic.w,
-    height: pic.h,
-    alt: pic.title,
-  }));
+    setCollection([
+      ...collection,
+      ...photoGalleryContainer.current.slice(length, length + qty),
+    ]);
+
+    setLength(length + qty);
+  };
+
+  // On mount
+  useEffect(() => {
+    // Create container for photoswipe
+    photoswipeContainer.current = pictures
+      .sort((a, b) => b.id - a.id)
+      .map(pic => {
+        const images = data.allImageSharp.edges;
+        const image = images.find(edge => edge.node.fluid.src.includes(pic.id));
+        return {
+          ...image,
+          src: image.node.fluid.originalImg,
+          title: formatTitle(pic),
+          w: pic.w,
+          h: pic.h,
+        };
+      });
+
+    // Create container for react-photo-gallery
+    photoGalleryContainer.current = photoswipeContainer.current.map(pic => ({
+      src: pic.node.fluid.src,
+      srcSet: pic.node.fluid.srcSet,
+      sizes: pic.node.fluid.sizes,
+      width: pic.w,
+      height: pic.h,
+      alt: pic.title,
+    }));
+
+    addPictures(10);
+  }, []);
 
   // Photoswipe trigger method
   const openGallery = (event, current) => {
@@ -53,7 +77,12 @@ const PicturesPage = ({ data, location }) => {
       history: false,
     };
 
-    const gallery = new PhotoSwipe(pswp, PhotoSwipeUI_Default, photoswipeContainer, options);
+    const gallery = new PhotoSwipe(
+      pswp,
+      PhotoSwipeUIDefault,
+      photoswipeContainer.current,
+      options
+    );
     gallery.init();
   };
 
@@ -63,19 +92,44 @@ const PicturesPage = ({ data, location }) => {
 
       <div className="container-fluid">
         <div className="ml-md-4">
-          <Fade><h1>Pictures</h1></Fade>
+          <Fade>
+            <h1>Pictures</h1>
+          </Fade>
 
           <Fade bottom distance="30px" cascade>
             <div className="mt-2">
-              <div style={{ margin: currentWidth > 990 ? '0 -45px' : '0 -5px' }}>
-                <PhotoGallery
-                  photos={photoGalleryContainer}
-                  onClick={openGallery}
-                  margin={currentWidth > 990 ? 50 : 10}
-                  columns={3}
-                  targetRowHeight={currentWidth > 990 ? 220 : 150}
-                  direction="row"
-                />
+              <div
+                style={{ margin: currentWidth > 990 ? '0 -45px' : '0 -5px' }}
+              >
+                {photoGalleryContainer.current.length > 0 && (
+                  <InfiniteScroll
+                    dataLength={collection.length}
+                    next={() => addPictures(10)}
+                    hasMore={
+                      photoGalleryContainer.current.length !== collection.length
+                    }
+                    loader={
+                      <div className="text-center">
+                        <Loader
+                          type="TailSpin"
+                          color="#000"
+                          height={40}
+                          width={40}
+                        />
+                      </div>
+                    }
+                  >
+                    {console.log(collection.length, length)}
+                    <PhotoGallery
+                      photos={collection}
+                      onClick={openGallery}
+                      margin={currentWidth > 990 ? 50 : 10}
+                      columns={3}
+                      targetRowHeight={currentWidth > 990 ? 220 : 150}
+                      direction="row"
+                    />
+                  </InfiniteScroll>
+                )}
               </div>
             </div>
           </Fade>
